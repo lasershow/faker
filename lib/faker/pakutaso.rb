b96d6eadb11997a5245b2b81f81639e78c48e4d0
Akihiro Nakao <genn777f3@gmail.com>
2016-10-30 11:46:43 +0900

    test commit

---
+++ lib/faker/pakutaso.rb
@@ -0,0 +1,9 @@
+module Faker
+  class Pakutaso < Base
+    class << self
+      def saya
+        "https://www.pakutaso.com/assets_c/2016/02/model_akanesaya-thumb-320x320-20922.jpg"
+      end
+    end
+  end
+end
